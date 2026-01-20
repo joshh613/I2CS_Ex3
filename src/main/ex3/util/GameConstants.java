56340887ae9955c2045cf47d31bc9e8916e25a44
Joshua Hall <github@joshuajacobhall.com>
2026-01-21 01:45:12 +0200

    refactor (lots of changes...)

--- src/main/ex3/util/GameConstants.java
+++ src/main/ex3/util/GameConstants.java
@@ -7,24 +7,13 @@ import java.awt.Color;
 public final class GameConstants {
     public static final int CODE = 0;
 
-    public static final int EMPTY;
-    public static final int WALL;
-    public static final int DOT;
-    public static final int POWERUP;
-    public static final int STAY;
-    public static final int UP, LEFT, DOWN, RIGHT;
+    public static final int EMPTY = Game.getIntColor(Color.BLACK, CODE);
+    public static final int WALL = Game.getIntColor(Color.BLUE, CODE);
+    public static final int DOT = Game.getIntColor(Color.PINK, CODE);
+    public static final int POWERUP = Game.getIntColor(Color.GREEN, CODE);
 
-    static {
-        EMPTY = Game.getIntColor(Color.BLACK, CODE);
-        WALL = Game.getIntColor(Color.BLUE, CODE);
-        DOT = Game.getIntColor(Color.PINK, CODE);
-        POWERUP = Game.getIntColor(Color.GREEN, CODE);
-        STAY = Game.STAY;
-        UP = Game.UP;
-        LEFT = Game.LEFT;
-        DOWN = Game.DOWN;
-        RIGHT = Game.RIGHT;
-    }
+    public static final int STAY = Game.STAY;
+    public static final int UP = Game.UP, LEFT = Game.LEFT, DOWN = Game.DOWN, RIGHT = Game.RIGHT;
 
     public static final int[] DIRS = {UP, LEFT, DOWN, RIGHT};
 
